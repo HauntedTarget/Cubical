a220c1e0f25df1e0013020d34badc4fcd0617f01
Haunted Target <75811611+HauntedTarget@users.noreply.github.com>
2021-07-26 20:57:03 +0000

    Took out the ability choose a seed for the time being and instead made the game choose the seed itself.

--- main.py
+++ main.py
@@ -31,38 +31,13 @@ class Game:
   #New Game Command
   def newGame(self):
     self.start.withdraw()
-    self.seedChoose()
+    random.seed(random.randint(10000000,100000000))
+    self.office()
   
   def loadGame(self):
     print("TEST")
   
-  def seedChoose(self):
-    #The window that asks for the user to type a seed or play a random one
-    self.seedWindow = tkin.Toplevel()
-    self.start.title('Cubical')
-    self.seedWindow.geometry('200x90')
-    self.seedWindow.iconphoto(False, self.icon)
-    self.box = tkin.Frame(self.seedWindow)
-    self.seed = tkin.Entry(self.box)
-    self.seed.insert(0,'Set Seed')
-    self.useSeed = tkin.Button(self.box,text='Use Seed',command=self.seedStart)
-    self.randomSeed = tkin.Button(self.box,text='Random Seed',command=self.randomStart)
-    #Packs the window
-    self.seed.pack(side='top')
-    self.useSeed.pack(side='top')
-    self.randomSeed.pack(side='top')
-    self.box.pack(side='top')
-
-  def seedStart(self):
-    try:
-      seed = int(self.seed.get())
-      random.seed(seed)
-    except:
-      random.seed(random.randint(10000000,100000000))
-    self.seedWindow.destroy()
-    print('TEST')
-  
-  def randomStart(self):
+  def office(self):
     print('TEST')
 
 
